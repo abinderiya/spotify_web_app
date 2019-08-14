c1f68c19abcd01de1aa60bac7aa9d57a7443ad9d
Binderiya Adishaa <b.adishaa@mail.utoronto.ca>
2019-08-14 10:58:25 -0400

    Fixed bug

--- spotify_app/settings.py
+++ spotify_app/settings.py
@@ -20,13 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0(3mphp4w*os&a99@2xzzkf6$)wk1ex(kc8*_bi8u59v2-qms('
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '0(3mphp4w*os&a99@2xzzkf6$)wk1ex(kc8*_bi8u59v2-qms(')
+SECRET_KEY = ''
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['https://playlist-visualizer.herokuapp.com',]
+ALLOWED_HOSTS = ['playlist-visualizer.herokuapp.com',]
 
 
 # Application definition
